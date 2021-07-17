a3a76f4e4fa272008ba99f36d47952797492c07c
Oğuzhan Tezcan <aslanozi55@gmail.com>
2021-07-17 11:49:05 +0300

    recipent selection screen added

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ class WindowMain:
         self.builder.connect_signals(self)
 
         self.windowMain = self.builder.get_object('window')
+        self.windowRecipent = self.builder.get_object('selectRecipentWindow')
+
         self.textView1 = self.builder.get_object('textview1')
+        self.recipentList = self.builder.get_object('combo1')
+        self.rec_name = self.builder.get_object('label_recipent_name')
+        self.rec_id = self.builder.get_object('label_recipent_id')
+
         self.windowMain.show()
     
     def on_window_destroy(self, widget, data=None):
@@ -49,9 +55,14 @@ class WindowMain:
             dialog.destroy()
 
 
-            recipents = SelectRecipent(self)
-            response = recipents.run()
-            recipents.destroy()
+            self.windowRecipent.show()
+    
+    def on_selectRecipentWindow_show(self,widget,data=None):
+        print('SHOW SIGNAL TRIGGER')
+
+    def on_proceedButton1_clicked(self, widget,data=None):
+        print("PROCEED CALL")
+        self.windowRecipent.hide()
 
         
     def decrypt(self,pgp_data):
@@ -65,20 +76,20 @@ class WindowMain:
         Gtk.main()
 
 
-class SelectRecipent(Gtk.Dialog):
-    def __init__(self, parent):
-        super().__init__(title="Select recipent", flags=0)
-        self.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
-        )
+# class SelectRecipent(Gtk.Dialog):
+#     def __init__(self):
+#         self.builder = Gtk.Builder()
+#         self.builder.add_from_file("gpg.glade")
+#         self.builder.connect_signals(self)
 
-        self.set_default_size(150, 100)
+#         self.windowRecipent = self.builder.get_object('selectRecipentWindow')
+#         self.windowRecipent.show()
 
-        label = Gtk.Label(label="Select recipent from list below")
+#     def on_proceedButton1_clicked(self, widget,data=None):
+#         print("PROCEED CALL")
 
-        box = self.get_content_area()
-        box.add(label)
-        self.show_all()
+#     def main(self):
+#         Gtk.main()
 
 
 if __name__ == "__main__":
